c74ee45d49c4b6c64cab95966338e1eabcba3d99
P. Mohan <pmverma.cs@gmail.com>
2019-07-26 10:33:52 +0630

    Create configure-docker.cmd

---
+++ configure-docker.cmd
@@ -0,0 +1,12 @@
+echo {"experimental": true} > "C:\ProgramData\Docker\config\daemon.json"
+net stop docker
+net stop com.docker.service
+taskkill /IM "dockerd.exe" /F
+taskkill /IM "Docker for Windows.exe" /F
+choco install docker-desktop -y
+set PATH="%PATH%;C:\Program Files\Docker\Docker"
+setx /m PATH="%PATH%;C:\Program Files\Docker\Docker"
+net start docker
+net start com.docker.service
+
+docker info
