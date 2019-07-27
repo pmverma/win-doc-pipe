51d4257795a8d313beee8cf17d168f6b0f8847c6
P. Mohan <pmverma.cs@gmail.com>
2019-07-27 12:36:52 +0630

    Update configure-docker.cmd

--- configure-docker.cmd
+++ configure-docker.cmd
@@ -1,17 +1,6 @@
 @ECHO ON
-echo {"experimental": true} > "C:\ProgramData\Docker\config\daemon.json"
-net stop docker
-net stop com.docker.service
-taskkill /IM "dockerd.exe" /F
-taskkill /IM "Docker for Windows.exe" /F
-choco install docker-desktop -y
-set PATH "%PATH%;C:\Program Files\Docker\Docker"
-sleep 3
-setx /m PATH "%PATH%;C:\Program Files\Docker\Docker"
-sleep 3
-net start docker
-net start com.docker.service
 
+C:
 cd "C:\Program Files\"
 mkdir "Linux Containers"
 cd ".\Linux Containers\"
