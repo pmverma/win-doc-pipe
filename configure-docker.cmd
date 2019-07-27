104349b43e663392bbadb95530d976d7d488b7ac
P. Mohan <pmverma.cs@gmail.com>
2019-07-27 12:21:09 +0630

    Update configure-docker.cmd

--- configure-docker.cmd
+++ configure-docker.cmd
@@ -18,6 +18,9 @@ cd ".\Linux Containers\"
 curl -LO https://github.com/linuxkit/lcow/releases/download/v4.14.35-v0.3.9/release.zip
 unzip release.zip
 
+echo PPPpppppppppppppppppppp
+cd
+
 dir "C:\Program Files\Linux Containers\"
 
 docker info
